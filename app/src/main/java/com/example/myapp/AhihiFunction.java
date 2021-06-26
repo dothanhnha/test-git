7a6981a44d78cffe949d2d5523319d6d1005d85b
Nha <dothanhnha1611@gmail.com>
2021-06-26 19:47:06 +0700

    resolve conflice

---
+++ app/src/main/java/com/example/myapp/AhihiFunction.java
@@ -0,0 +1,12 @@
+package com.example.myapp;
+
+public class AhihiFunction {
+    public int getSum(int a, int b){
+        return a+b;
+    }
+
+    public int getSub(int a, int b){
+        return a-b;
+    }
+
+}
