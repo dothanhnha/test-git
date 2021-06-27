2c546c76e13e84b855df3ff764bf049dc2c33a7c
Nha <dothanhnha1611@gmail.com>
2021-06-27 19:23:52 +0700

    commit 3

--- app/src/main/java/com/example/myapp/MainActivity.java
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        String a = "amendcommit";
+        String a = "amend             commit";
         setContentView(R.layout.activity_main);
     }
 
